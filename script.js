fdefdc90b7563dcca1348f420020469abd7fb788
Firdosh Jamadar <firdoshjamadar0@gmail.com>
2025-02-11 11:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	Constructor(name, age){
+		this.name = name;
+		this.age= age;
+		
+	}
+}
 
-class Student extends Person {}
+get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+set age() {
+        return this._age;
+    }
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+
 
 // Do not change the code below this line
 window.Person = Person;
