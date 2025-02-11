b0edcd7e18b9f73c00fefbf86b86e7653ee8ecb2
Firdosh Jamadar <firdoshjamadar0@gmail.com>
2025-02-11 12:25:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-//complete this code
 class Person {
-	Constructor(name, age){
-		this.name = name;
-		this.age= age;
-		
-	}
-}
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-get name() {
+    get name() {
         return this._name;
     }
 
-set age(age) {
-        return this._age;
+    set age(age) {
+        if (age > 0) {
+            this._age = age;
+        } else {
+            console.log("Age must be a positive number.");
+        }
     }
+}
+
 class Student extends Person {
     study() {
         console.log(`${this.name} is studying`);
     }
 }
 
-
 class Teacher extends Person {
     teach() {
         console.log(`${this.name} is teaching`);
     }
 }
+
 
 
 // Do not change the code below this line
